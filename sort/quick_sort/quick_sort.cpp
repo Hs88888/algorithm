107264034d5c390eee81d7dd59179a7f3f4e8d2c
H S <1003597101@qq.com>
2022-08-17 23:52:35 +0800

    modify quickSort

--- sort/quick_sort/quick_sort.cpp
+++ sort/quick_sort/quick_sort.cpp
@@ -10,21 +10,25 @@ const int N = 100010;
 
 int q[N];
 
-void quick_sort(int q[], int l, int r)
-{
-    if (l >= r) {
-        return;
-    }
+void quickSort(int q[], int l, int r) {
+    if (l >= r) return;
 
     int i = l - 1, j = r + 1, x = q[l + r >> 1];
     while (i < j) {
-        do i++; while (q[i] < x);
-        do j--; while (q[j] > x);
-        if (i < j) swap(q[i], q[j]);
+        do {
+            ++i;
+        } while (q[i] < x);
+        do {
+            --j;
+        } while (q[j] > x);
+
+        if (i < j) {
+            swap(q[i], q[j]);
+        }
     }
 
-    quick_sort(q, l, j);
-    quick_sort(q, j+1, r);
+    quickSort(q, l, j);
+    quickSort(q, j + 1, r);
 }
 
 int main() {
@@ -35,7 +39,7 @@ int main() {
         scanf("%d", &q[i]);
     }
 
-    quick_sort(q, 0, n - 1);
+    quickSort(q, 0, n - 1);
 
     for (int i = 0; i < n; i++) {
         printf("%d ", q[i]);
