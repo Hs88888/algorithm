46edbdd4881fec668a553d96f139f87797e32f3f
H S <1003597101@qq.com>
2023-11-14 21:17:59 +0800

    quick sort optimization

--- sort/quick_sort/quick_sort.go
+++ sort/quick_sort/quick_sort.go
@@ -11,23 +11,21 @@ package main
 import "fmt"
 
 func main() {
-	arr := []int{1, 5, 7, 4, 3, 3}
+	arr := []int{1, 3, 7, 5, 4, 3, 3}
 	quickSort(arr)
 	fmt.Println("arr: ", arr)
 }
 
-func quickSort(arr []int) []int {
-	return _quickSort(arr, 0, len(arr)-1)
+func quickSort(arr []int) {
+	_quickSort(arr, 0, len(arr)-1)
 }
 
-func _quickSort(arr []int, left, right int) []int {
-	if left < right {
+func _quickSort(arr []int, left, right int) {
+	if right > left {
 		partitionIndex := partition(arr, left, right)
 		_quickSort(arr, left, partitionIndex-1)
 		_quickSort(arr, partitionIndex+1, right)
 	}
-
-	return arr
 }
 
 func partition(arr []int, left, right int) int {
@@ -35,11 +33,12 @@ func partition(arr []int, left, right int) int {
 	index := left + 1
 
 	for i := index; i <= right; i++ {
-		if arr[i] < arr[pivot] {
+		if arr[i] <= arr[pivot] {
 			swap(arr, index, i)
 			index++
 		}
 	}
+
 	swap(arr, pivot, index-1)
 
 	return index-1
